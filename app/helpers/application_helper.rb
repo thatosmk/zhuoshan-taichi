9de901ea778745c9eaf94ec003e6e412d8ae9158
Thato Semoko <thatosmk@gmail.com>
2020-01-23 13:05:32 +0200

    adds landing page content and design

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+  def title text
+    content_for(:title) { text }
+  end
+
+  def meta_tag tags, text
+    content_for :"meta_#{tags}", text 
+  end
+
+  def yield_meta_tag tags, default_text='' 
+    content_for?(:"meta_#{tags}") ? content_for(:"meta_#{tags}") : default_text
+  end
 end
