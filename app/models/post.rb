ceb592e01251bc9e6ca52a0d65ddc74a09b8d624
Thato Semoko <thatosmk@gmail.com>
2020-03-17 08:10:58 +0200

    adds slugs for posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,13 @@
 class Post < ApplicationRecord
   belongs_to :user
+  extend FriendlyId
+  friendly_id :slug_options, use: :slugged
+
+
+  def slug_options
+    [
+      [:title],
+      [:title, :created_at]
+    ]
+  end
 end
