2a21962acdbea03affed8a538937f6eea190ed59
Thato Semoko <thatosmk@gmail.com>
2020-04-09 10:30:28 +0200

    adds mail config to codebase

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,11 @@ module Taichi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.secret_key_base = Figaro.env.secret_key_base
 
+    Raven.configure do |config|
+      config.dsn = 'https://18976bf6d09847bc9c9002f57a158172:0a6e1172edae4ced842cb7767bc4220f@sentry.io/1822019'
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
